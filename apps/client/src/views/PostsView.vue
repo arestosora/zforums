47e17c4e9326ffa97332304200b133d1e02f99f3
Ares <89419093+arestosora@users.noreply.github.com>
2024-07-02 17:59:17 -0500

    feat: posts services

--- apps/client/src/views/PostsView.vue
+++ apps/client/src/views/PostsView.vue
@@ -4,12 +4,10 @@
     <div class="sidebar-container">
       <SidebarComponent class="sidebar-component" />
     </div>
-    
     <!-- Main Container -->
     <div class="flex flex-col lg:flex-row justify-center items-start w-full max-w-screen-lg mx-auto mt-4 lg:mt-0">
       <!-- Loading component -->
       <LoadingComponent :visible="isLoading" />
-
       <!-- Main Content -->
       <div class="main-content flex-1 flex custom-width mx-auto">
         <!-- Posts Wrapper -->
@@ -85,204 +83,33 @@
 </template>
 
 <script setup lang="ts">
-import { ref, onMounted, computed } from 'vue';
-import axios from 'axios';
-import { authState } from '../utils/auth';
-import { format } from 'date-fns';
-import type { Post } from '../types/Post';
+import { authState } from '@/utils/auth';
+import {
+  onMounted } from 'vue';
+import {
+  isLoading,
+  posts,
+  newPostContent,
+  newPostImageUrl,
+  fileInput,
+  fetchPosts,
+  sortedPosts,
+  formatDate,
+  createPost,
+  deletePost,
+  editPost,
+  toggleLike,
+  triggerFileInput,
+  handleFileUpload,
+  navigateToComments,
+  sharePost
+} from '../services/posts.service';
 import SidebarComponent from '@/components/SidebarComponent.vue';
-import { useRouter } from 'vue-router';
 import NewsComponent from '@/components/NewsComponent.vue';
-import { showErrorAlert, showSuccessAlert } from '@/utils/fireAlert';
 import LoadingComponent from '@/components/LoadingComponent.vue';
-import Swal from 'sweetalert2';
-const isLoading = ref(false);
-const router = useRouter();
-const posts = ref<Post[]>([]);
-const newPostContent = ref('');
-const newPostImageUrl = ref('');
-const fileInput = ref<HTMLInputElement | null>(null);
 
-onMounted(async () => {
-  isLoading.value = true;
-  if (!authState.token) {
-    showErrorAlert('Session Expired. Please log in again.');
-    isLoading.value = false;
-    return;
-  }
-
-  try {
-    const response = await axios.get('/posts', {
-      headers: {
-        'Accept': 'application/json',
-        'Authorization': `Bearer ${authState.token}`
-      }
-    });
-    posts.value = response.data.map((post: Post) => ({
-      ...post,
-      liked: false
-    })).sort((a: Post, b: Post) => new Date(b.createdAt!).getTime() - new Date(a.createdAt!).getTime());
-    console.log('Data received:', response.data);
-  } catch (err) {
-    showErrorAlert('Error fetching posts. Please try again later.');
-    console.error(err);
-  } finally {
-    isLoading.value = false;
-  }
-});
-
-const sortedPosts = computed(() => {
-  return posts.value.slice().sort((a, b) => new Date(b.createdAt!).getTime() - new Date(a.createdAt!).getTime());
-});
-
-const formatDate = (dateStr: string) => {
-  return format(new Date(dateStr), 'dd/MM/yyyy HH:mm');
-};
-
-const createPost = async () => {
-  if (!newPostContent.value) return;
-
-  isLoading.value = true;
-  try {
-    const response = await axios.post('/posts', {
-      title: 'New Post',
-      content: newPostContent.value,
-      imageUrl: newPostImageUrl.value
-    }, {
-      headers: {
-        'Authorization': `Bearer ${authState.token}`
-      }
-    });
-
-    posts.value.unshift({
-      ...response.data,
-      liked: false
-    });
-    newPostContent.value = '';
-    newPostImageUrl.value = '';
-    setTimeout(() => {
-      location.reload();
-    }, 500);
-  } catch (err) {
-    showErrorAlert('Error creating post. Please try again later.');
-    console.error(err);
-  } finally {
-    isLoading.value = false;
-  }
-};
-
-const deletePost = async (postId: number) => {
-  isLoading.value = true;
-  try {
-    await axios.delete(`/posts/${postId}`, {
-      headers: {
-        'Authorization': `Bearer ${authState.token}`
-      }
-    });
-    posts.value = posts.value.filter(post => post.id !== postId);
-    showSuccessAlert('Post deleted successfully');
-  } catch (err) {
-    showErrorAlert('Error deleting post. Please try again later.');
-    console.error(err);
-  } finally {
-    isLoading.value = false;
-  }
-};
-
-const editPost = (postId: number) => {
-  Swal.fire({
-    icon: 'info',
-    title: 'Edit Post',
-    text: 'Feature coming soon!',
-    showConfirmButton: false,
-    timer: 1000,
-    customClass: {
-      popup: 'swal2-popup',
-      title: 'swal2-title'
-    }
-  });
-};
-
-const toggleLike = async (post: Post) => {
-  isLoading.value = true;
-  try {
-    await axios.patch(`/posts/${post.id}/like`, {}, {
-      headers: {
-        'Authorization': `Bearer ${authState.token}`
-      }
-    });
-
-    post.liked = !post.liked;
-    post.likes! += post.liked ? 1 : -1;
-  } catch (err) {
-    showErrorAlert('Error liking post. Please try again later.');
-    console.error(err);
-  } finally {
-    isLoading.value = false;
-  }
-};
-
-const triggerFileInput = () => {
-  if (fileInput.value) {
-    fileInput.value.click();
-  }
-};
-
-const handleFileUpload = async (event: Event) => {
-  const file = (event.target as HTMLInputElement).files?.[0];
-  if (!file) return;
-
-  isLoading.value = true;
-  const formData = new FormData();
-  formData.append('file', file);
-
-  const uploadPreset = 'ljgz0ika';
-  const apiKey = '186842759432363';
-  const timestamp = Math.floor(Date.now() / 1000);
-
-  formData.append('upload_preset', uploadPreset);
-  formData.append('api_key', apiKey);
-  formData.append('timestamp', timestamp.toString());
-
-  try {
-    const response = await axios.post('https://api.cloudinary.com/v1_1/dijbgmxrh/image/upload', formData, {
-      headers: {
-        'Content-Type': 'multipart/form-data'
-      }
-    });
-
-    newPostImageUrl.value = response.data.secure_url;
-  } catch (err) {
-    showErrorAlert('Error uploading image. Please try again later.');
-    console.error(err);
-  } finally {
-    isLoading.value = false;
-  }
-};
-
-const navigateToComments = (postId: number) => {
-  router.push({ name: 'postComments', params: { id: postId } });
-};
-
-const sharePost = async (postId: number) => {
-  isLoading.value = true;
-  try {
-    await axios.post(`/posts/${postId}/share`, {}, {
-      headers: {
-        'Authorization': `Bearer ${authState.token}`
-      }
-    });
-
-    showSuccessAlert('Post shared successfully');
-  } catch (err) {
-    showErrorAlert('You have shared this post already.');
-    console.error(err);
-  } finally {
-    isLoading.value = false;
-  }
-};
+onMounted(fetchPosts);
 </script>
-
 <style scoped>
 @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@400;600&display=swap');
 
