2d3d5163a8c894162eed1c816228da433bbc6178
Ares <89419093+arestosora@users.noreply.github.com>
2024-07-02 17:41:39 -0500

    fix: axios environment variables read

--- apps/client/src/utils/axios.ts
+++ apps/client/src/utils/axios.ts
@@ -1,5 +1,4 @@
 import axios from 'axios'
 
-axios.defaults.baseURL = import.meta.env.VUE_APP_API_URL;
+axios.defaults.baseURL = `${import.meta.env.VITE_API_URL}/api/v1`;
 axios.defaults.headers.post['Content-Type'] = 'application/json';
-    
